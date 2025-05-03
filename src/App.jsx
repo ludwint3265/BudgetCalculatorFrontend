0d65531f6a7b621c4d2411bddd2d8932b53641aa
Mikadifo <mikadifopadillaheredia@gmail.com>
2025-05-03 11:56:37 -0400

    add: ai suggestion styles

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,21 @@
 import React, { useState } from "react";
 import logo from "/logo.svg";
 import DollarIcon from "./assets/icons/dollarIcon.svg?react";
+import SparkIcon from "./assets/icons/sparkIcon.svg?react";
 import { categories } from "./constants/categories";
 
 function App() {
   const [selectedCategory, setSelectedCategory] = useState(null);
   const [userBudget, setUserBudget] = useState(0);
+  const [aiSuggestion, setAiSuggestion] =
+    useState(`3–4 Day City Escape: Explore Lisbon, Portugal or Mexico City. Enjoy affordable flights, boutique hotels, and authentic local food — all within budget.
+
+	      Nature Getaway: Spend a relaxing long weekend in Colorado or British Columbia, focusing on hiking, local lodges, and scenic views.
+
+	      Budget Tip: Travel mid-week for cheaper flights and consider hostels or Airbnb stays for more savings.
+
+              Stretch Your Dollar: Use a travel rewards card to offset costs or book during off-season for lower rates.
+`);
 
   const handleBudgetChange = (event) => {
     const budget = event.target.value;
@@ -64,14 +74,34 @@ function App() {
       </div>
 
       {userBudget && selectedCategory ? (
-        <div className="mb-32">
+        <div className="mb-32 flex flex-col">
           <h1 className="text-[#010810] font-bold text-4xl mb-2">
             Your AI-Generated Suggestion
           </h1>
           <h2 className="text-[#010810] font-normal text-[28px]">
-            With a ${userBudget} budget, here are a few {selectedCategory}{" "}
-            options you might like:
+            With a <b>${userBudget}</b> budget, here are a few{" "}
+            <b>{selectedCategory}</b> options you might like:
           </h2>
+
+          <div className="relative mt-12 text-[#F7F9FA] w-full rounded-xl p-12 bg-[linear-gradient(to_bottom_right,_#F26A50_0%,_#D95778_20%,_#A9578F_40%,_#71598F_60%,_#425479_80%,_#2F4858_100%)] font-normal text-xl">
+            <SparkIcon className="absolute top-4 left-4" />
+            <span>{aiSuggestion}</span>
+          </div>
+
+          <div className="flex gap-8 mt-12 self-center">
+            <button
+              type="button"
+              className="bg-[#1C344B] rounded-xl py-3 px-8 text-white font-bold"
+            >
+              Show me more options
+            </button>
+            <button
+              type="button"
+              className="bg-[#F7F9FA] text-[#1C344B] border-2 border-[#1C344B] rounded-xl py-3 px-8 font-bold"
+            >
+              Choose another category
+            </button>
+          </div>
         </div>
       ) : (
         ""
