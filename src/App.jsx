9f4eb9cc7c7909314e7637d3ebaaccba7a62ee20
Mikadifo <mikadifopadillaheredia@gmail.com>
2025-05-03 11:38:47 -0400

    add: categories selection

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,19 @@
+import React, { useState } from "react";
 import logo from "/logo.svg";
 import DollarIcon from "./assets/icons/dollarIcon.svg?react";
+import { categories } from "./constants/categories";
 
 function App() {
+  const [selectedCategory, setSelectedCategory] = useState(null);
+  const [userBudget, setUserBudget] = useState(0);
+
+  const handleBudgetChange = (event) => {
+    const budget = event.target.value;
+    if (budget > 0) {
+      setUserBudget(budget);
+    }
+  };
+
   return (
     <div className="pt-10 px-32 flex flex-col">
       <div className="flex justify-center items-center gap-2 mb-16">
@@ -23,7 +35,7 @@ function App() {
             type="number"
             placeholder="1,500"
             className="font-bold text-[28px] border-2 border-[#1C344B] rounded-lg px-6 py-3 text-center w-[200px]"
-            required
+            onChange={handleBudgetChange}
           />
         </div>
       </div>
@@ -35,17 +47,35 @@ function App() {
         <h2 className="text-[#010810] font-normal text-[28px]">
           Choose how you want to make the most of your money
         </h2>
+
+        <div className="flex flex-wrap gap-12 mt-12">
+          {categories.map((category) => (
+            <button
+              type="button"
+              key={category.id}
+              className={`flex py-5 px-10 gap-3 rounded-xl border-2 border-[#1C344B] font-bold cursor-pointer text-[#1C344B] ${selectedCategory === category.name ? "bg-[#FDBD1D]" : "bg-[#F7F9FA] hover:bg-[#FFEEC4]"}`}
+              onClick={() => setSelectedCategory(category.name)}
+            >
+              {React.createElement(category.icon)}
+              {category.name}
+            </button>
+          ))}
+        </div>
       </div>
 
-      <div className="mb-32">
-        <h1 className="text-[#010810] font-bold text-4xl mb-2">
-          Your AI-Generated Suggestion
-        </h1>
-        <h2 className="text-[#010810] font-normal text-[28px]">
-          With a $${1500} budget, here are a few ${"category"} experiences you
-          might enjoy:
-        </h2>
-      </div>
+      {userBudget && selectedCategory ? (
+        <div className="mb-32">
+          <h1 className="text-[#010810] font-bold text-4xl mb-2">
+            Your AI-Generated Suggestion
+          </h1>
+          <h2 className="text-[#010810] font-normal text-[28px]">
+            With a ${userBudget} budget, here are a few {selectedCategory}{" "}
+            options you might like:
+          </h2>
+        </div>
+      ) : (
+        ""
+      )}
     </div>
   );
 }
---
+++ src/constants/categories.js
@@ -0,0 +1,23 @@
+import TravelIcon from "./../assets/icons/travelIcon.svg?react";
+import FoodIcon from "./../assets/icons/foodIcon.svg?react";
+import EducationIcon from "./../assets/icons/educationIcon.svg?react";
+import HomeIcon from "./../assets/icons/homeIcon.svg?react";
+import SavingIcon from "./../assets/icons/savingIcon.svg?react";
+import WellnessIcon from "./../assets/icons/wellnessIcon.svg?react";
+import GroupIcon from "./../assets/icons/groupIcon.svg?react";
+import FunIcon from "./../assets/icons/funIcon.svg?react";
+import ShieldIcon from "./../assets/icons/shieldIcon.svg?react";
+import SubscriptionIcon from "./../assets/icons/subscriptionIcon.svg?react";
+
+export const categories = [
+  { id: 1, name: "Travel", icon: TravelIcon },
+  { id: 2, name: "Food", icon: FoodIcon },
+  { id: 3, name: "Education / Courses", icon: EducationIcon },
+  { id: 4, name: "Housing / Rent Rate", icon: HomeIcon },
+  { id: 5, name: "Save & Grow It", icon: SavingIcon },
+  { id: 6, name: "Fitness & Wellness", icon: WellnessIcon },
+  { id: 7, name: "Social Impact & Charity", icon: GroupIcon },
+  { id: 8, name: "Entertainment", icon: FunIcon },
+  { id: 9, name: "Emergency Fund", icon: ShieldIcon },
+  { id: 10, name: "Subscriptions & Membership", icon: SubscriptionIcon },
+];
