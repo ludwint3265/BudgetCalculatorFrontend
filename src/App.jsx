b7d0798285cd9920b9c35bb5e08c969ef66dcbc5
Mikadifo <mikadifopadillaheredia@gmail.com>
2025-05-03 19:49:23 -0400

    fix: validations

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import React, { useState } from "react";
 import logo from "/logo.svg";
 import DollarIcon from "./assets/icons/dollarIcon.svg?react";
+import LocationIcon from "./assets/icons/locationIcon.svg?react";
 import SparkIcon from "./assets/icons/sparkIcon.svg?react";
 import AddIcon from "./assets/icons/addIcon.svg?react";
 import SparksIcon from "./assets/icons/sparkIcon.svg?react";
@@ -11,7 +12,7 @@ import { API_URL } from "./constants/API";
 import "./App.css";
 
 function App() {
-  const [selectedCategory, setSelectedCategory] = useState(null);
+  const [selectedCategories, setSelectedCategories] = useState([]);
   const [userBudget, setUserBudget] = useState(-1);
   const [userLocation, setUserLocation] = useState("");
   const [shake, setShake] = useState(false);
@@ -19,6 +20,7 @@ function App() {
   const [customCategory, setCustomCategory] = useState("");
   const [errorMessage, setErrorMessage] = useState("");
   const [loading, setLoading] = useState(false);
+  const [missingFields, setMissingFields] = useState(null);
 
   const handleBudgetChange = (event) => {
     const budget = event.target.value;
@@ -33,10 +35,14 @@ function App() {
     setUserLocation(location.trim());
   };
 
-  const handleCategoryChange = async (category) => {
-    if (selectedCategory !== category) {
+  const generateSuggestions = async () => {
+    if (
+      userBudget >= 0 &&
+      userLocation &&
+      selectedCategories.length > 0 &&
+      selectedCategories.length <= 3
+    ) {
       setLoading(true);
-      setSelectedCategory(category);
 
       try {
         const res = await fetch(`${API_URL}/get-suggestions`, {
@@ -47,7 +53,8 @@ function App() {
           },
           body: JSON.stringify({
             budget: userBudget,
-            category: category,
+            location: userLocation,
+            category: selectedCategories[0],
           }),
         });
 
@@ -66,6 +73,19 @@ function App() {
       } finally {
         setLoading(false);
       }
+    } else {
+      setMissingFields(true);
+    }
+  };
+
+  const handleCategoryChange = (category) => {
+    if (selectedCategories.includes(category)) {
+      setSelectedCategories(selectedCategories.filter((c) => c !== category));
+      return;
+    }
+
+    if (selectedCategories.length < 3) {
+      setSelectedCategories([...selectedCategories, category]);
     }
   };
 
@@ -81,7 +101,10 @@ function App() {
   const handleAddCustomCategory = () => {
     if (
       customCategory.trim() &&
-      !categories.some((category) => category.name === customCategory)
+      !categories.includes(
+        (category) =>
+          category.name.toLowerCase() === customCategory.toLowerCase(),
+      )
     ) {
       const newCategory = {
         id: categories.length + 1,
@@ -90,7 +113,7 @@ function App() {
       };
       categories.push(newCategory);
       setCustomCategory("");
-      setSelectedCategory(customCategory);
+      setSelectedCategories(customCategory);
       handleCategoryChange(customCategory);
     } else {
       setCustomCategory("");
@@ -126,7 +149,7 @@ function App() {
             />
           </div>
           <div className="flex items-center gap-2">
-            <DollarIcon />
+            <LocationIcon />
             <input
               type="text"
               placeholder="New York City"
@@ -142,162 +165,166 @@ function App() {
           Let’s explore your possibilities
         </h1>
         <div className="flex flex-col gap-6 mt-12 text-[#D95759] font-bold text-[28px]">
-          {userBudget < 0 || !userBudget ? (
-            <div className="flex flex-col text-[#D95759] font-bold text-[28px]">
-              <div className="flex gap-2 items-center">
-                <ErrorIcon />
-                <span>
-                  Please enter your budget first. Budget must be a number
-                  greater than or equal to 0.
-                </span>
-              </div>
+          <h2 className="text-[#010810] font-normal text-[28px]">
+            Choose how you want to make the most of your money
+          </h2>
+          <div className="flex flex-wrap gap-12 mt-12">
+            {categories.map((category) => (
+              <button
+                type="button"
+                key={category.id}
+                className={`flex py-5 px-10 gap-3 rounded-xl border-2 border-[#1C344B] font-bold cursor-pointer items-center text-[#1C344B] ${selectedCategories.includes(category.name) ? "bg-[#FDBD1D]" : "bg-[#F7F9FA] hover:bg-[#FFEEC4]"}`}
+                onClick={() => handleCategoryChange(category.name)}
+              >
+                {React.createElement(category.icon)}
+                {category.name}
+              </button>
+            ))}
+            <div className="flex text-[#1C344B] border-2 border-[#1C344B] rounded-xl font-bold items-center">
+              <input
+                type="text"
+                placeholder="Custom Category"
+                className="py-5 px-10 border-e-2 w-[216px] h-full rounded-l-xl"
+                value={customCategory}
+                onChange={(e) => setCustomCategory(e.target.value)}
+              />
+              <button
+                className={`flex gap-2 cursor-pointer bg-[#FDBD1D] py-5 px-3 rounded-r-xl h-full items-center ${shake ? "shake-animation" : ""}`}
+                onClick={handleAddCustomCategory}
+              >
+                <span>Add</span>
+                <AddIcon />
+              </button>
             </div>
-          ) : (
-            ""
-          )}
-          {!userLocation ? (
-            <div className="flex flex-col text-[#D95759] font-bold text-[28px]">
-              <div className="flex gap-2 items-center">
-                <ErrorIcon />
-                <span>
-                  Please enter your preferred location — it can be a country,
-                  city, or any specific place.
-                </span>
-              </div>
-            </div>
-          ) : (
-            ""
-          )}
-          {userBudget >= 0 && userLocation && (
-            <>
-              <h2 className="text-[#010810] font-normal text-[28px]">
-                Choose how you want to make the most of your money
-              </h2>
-
-              <div className="flex flex-wrap gap-12 mt-12">
-                {categories.map((category) => (
-                  <button
-                    type="button"
-                    key={category.id}
-                    className={`flex py-5 px-10 gap-3 rounded-xl border-2 border-[#1C344B] font-bold cursor-pointer items-center text-[#1C344B] ${selectedCategory === category.name ? "bg-[#FDBD1D]" : "bg-[#F7F9FA] hover:bg-[#FFEEC4]"}`}
-                    onClick={() => handleCategoryChange(category.name)}
-                  >
-                    {React.createElement(category.icon)}
-                    {category.name}
-                  </button>
-                ))}
-                <div className="flex text-[#1C344B] border-2 border-[#1C344B] rounded-xl font-bold items-center">
-                  <input
-                    type="text"
-                    placeholder="Custom Category"
-                    className="py-5 px-10 border-e-2 w-[216px] h-full rounded-l-xl"
-                    value={customCategory}
-                    onChange={(e) => setCustomCategory(e.target.value)}
-                  />
-                  <button
-                    className={`flex gap-2 cursor-pointer bg-[#FDBD1D] py-5 px-3 rounded-r-xl h-full items-center ${shake ? "shake-animation" : ""}`}
-                    onClick={handleAddCustomCategory}
-                  >
-                    <span>Add</span>
-                    <AddIcon />
-                  </button>
-                </div>
-              </div>
-            </>
-          )}
+          </div>
+          <button
+            type="button"
+            className="bg-[#1C344B] rounded-xl py-3 px-8 text-white font-bold cursor-pointer mt-12 self-center"
+            onClick={generateSuggestions}
+          >
+            Generate Suggestions
+          </button>
         </div>
       </div>
 
       <div className="mb-32 flex flex-col">
-        <h1 className="text-[#010810] font-bold text-4xl mb-2">
-          Your AI-Generated Suggestions
-        </h1>
-        {userBudget >= 0 && selectedCategory && !errorMessage ? (
-          loading ? (
-            <div className="flex items-center gap-2 justify-center mt-12">
-              <img
-                src={logo}
-                alt="PennyPath Logo"
-                className="w-8 h-8 animate-spin"
-                style={{ animationDuration: "3s" }}
-              />
-              <span className="font-bold text-2xl text-[#1C344B]">
-                Generating your personalized options...
-              </span>
-            </div>
-          ) : (
-            <>
-              <h2 className="text-[#010810] font-normal text-[28px]">
-                With a{" "}
-                <b className="underline decoration-[#FDBD1D] decoration-4 underline-offset-8">
-                  ${userBudget}
-                </b>{" "}
-                budget, here are some {userBudget > 0 ? " " : "free "}
-                <b className="underline decoration-[#FDBD1D] decoration-4 underline-offset-8">
-                  {selectedCategory}
-                </b>{" "}
-                options you might like:
-              </h2>
-
-              <div className="flex flex-col gap-8 mt-12">
-                {aiSuggestions.map((suggestion, index) => (
-                  <div
-                    key={index}
-                    className="relative text-[#F7F9FA] w-full rounded-xl p-12 bg-[linear-gradient(to_bottom_right,_#F26A50_0%,_#D95778_20%,_#A9578F_40%,_#71598F_60%,_#425479_80%,_#2F4858_100%)] font-normal text-xl"
-                  >
-                    <SparkIcon className="absolute top-4 right-4" />
-                    <span>{suggestion}</span>
-                  </div>
-                ))}
-              </div>
-
-              <div className="flex gap-8 mt-12 self-center">
-                <button
-                  type="button"
-                  className="bg-[#1C344B] rounded-xl py-3 px-8 text-white font-bold cursor-pointer"
-                  onClick={handleMoreOptions}
-                >
-                  Show me more options
-                </button>
-                <button
-                  type="button"
-                  className="bg-[#F7F9FA] text-[#1C344B] border-2 border-[#1C344B] rounded-xl py-3 px-8 font-bold cursor-pointer"
-                  onClick={() => {
-                    setSelectedCategory("");
-                    setAiSuggestions([]);
-                  }}
-                >
-                  Choose another category
-                </button>
-              </div>
-            </>
-          )
-        ) : (
-          <div className="flex flex-col gap-6 mt-12 text-[#D95759] font-bold text-[28px]">
-            {!selectedCategory ? (
-              <div className="flex gap-2 items-center">
-                <ErrorIcon />
-                <span>
-                  Please select at least one category and up to 3, or add your
-                  own.
-                </span>
-              </div>
-            ) : (
-              ""
-            )}
-
-            {errorMessage ? (
-              <div className="flex gap-2 items-center">
-                <ErrorIcon />
-                <span>
-                  Something went wrong while generating suggestions. Please try
-                  again later.
-                </span>
-              </div>
-            ) : (
-              ""
-            )}
+        {loading ? (
+          <div className="flex items-center gap-2 justify-center mt-12">
+            <img
+              src={logo}
+              alt="PennyPath Logo"
+              className="w-8 h-8 animate-spin"
+              style={{ animationDuration: "3s" }}
+            />
+            <span className="font-bold text-2xl text-[#1C344B]">
+              Generating your personalized options...
+            </span>
           </div>
+        ) : (
+          ""
+        )}
+
+        {aiSuggestions.length > 0 && !errorMessage ? (
+          <>
+            <h1 className="text-[#010810] font-bold text-4xl mb-2">
+              Your AI-Generated Suggestions
+            </h1>
+            <h2 className="text-[#010810] font-normal text-[28px]">
+              With a{" "}
+              <b className="underline decoration-[#FDBD1D] decoration-4 underline-offset-8">
+                ${userBudget}
+              </b>{" "}
+              budget, here are some {userBudget > 0 ? " " : "free "}
+              <b className="underline decoration-[#FDBD1D] decoration-4 underline-offset-8">
+                {selectedCategories}
+              </b>{" "}
+              options you might like:
+            </h2>
+
+            <div className="flex flex-col gap-8 mt-12">
+              {aiSuggestions.map((suggestion, index) => (
+                <div
+                  key={index}
+                  className="relative text-[#F7F9FA] w-full rounded-xl p-12 bg-[linear-gradient(to_bottom_right,_#F26A50_0%,_#D95778_20%,_#A9578F_40%,_#71598F_60%,_#425479_80%,_#2F4858_100%)] font-normal text-xl"
+                >
+                  <SparkIcon className="absolute top-4 right-4" />
+                  <span>{suggestion}</span>
+                </div>
+              ))}
+            </div>
+
+            <div className="flex gap-8 mt-12 self-center">
+              <button
+                type="button"
+                className="bg-[#1C344B] rounded-xl py-3 px-8 text-white font-bold cursor-pointer"
+                onClick={handleMoreOptions}
+              >
+                Show me more options
+              </button>
+              <button
+                type="button"
+                className="bg-[#F7F9FA] text-[#1C344B] border-2 border-[#1C344B] rounded-xl py-3 px-8 font-bold cursor-pointer"
+                onClick={() => {
+                  setSelectedCategories("");
+                  setAiSuggestions([]);
+                }}
+              >
+                Choose another category
+              </button>
+            </div>
+          </>
+        ) : (
+          missingFields && (
+            <div className="flex flex-col gap-6 mt-12 text-[#D95759] font-bold text-[28px]">
+              {!userBudget || userBudget < 0 ? (
+                <div className="flex gap-2 items-center">
+                  <ErrorIcon />
+                  <span>
+                    Please enter your budget first. Budget must be a number
+                    greater than or equal to 0.
+                  </span>
+                </div>
+              ) : (
+                ""
+              )}
+
+              {!userLocation ? (
+                <div className="flex gap-2 items-center">
+                  <ErrorIcon />
+                  <span>
+                    Please enter your preferred location — it can be a country,
+                    city, or any specific place.
+                  </span>
+                </div>
+              ) : (
+                ""
+              )}
+
+              {!selectedCategories.length || selectedCategories.length > 3 ? (
+                <div className="flex gap-2 items-center">
+                  <ErrorIcon />
+                  <span>
+                    Please select at least one category and up to 3, or add your
+                    own.
+                  </span>
+                </div>
+              ) : (
+                ""
+              )}
+
+              {errorMessage ? (
+                <div className="flex gap-2 items-center">
+                  <ErrorIcon />
+                  <span>
+                    Something went wrong while generating suggestions. Please
+                    try again later.
+                  </span>
+                </div>
+              ) : (
+                ""
+              )}
+            </div>
+          )
         )}
       </div>
     </div>
