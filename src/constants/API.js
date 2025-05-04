d9f1f63296390edb499cd2dfe94fa3efc28ba939
Mikadifo <mikadifopadillaheredia@gmail.com>
2025-05-03 22:19:32 -0400

    mod: updated api link

--- src/constants/API.js
+++ src/constants/API.js
@@ -1,2 +1,2 @@
 export const API_DEV = "http://localhost:5000/";
-export const API_URL = "https://budgetcalculatorbackend.onrender.com/";
+export const API_URL = "https://budgetcalculatorbackend-4.onrender.com/";
